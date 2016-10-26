369ed9fa057e59127f8614f8401fc0daf46d89e0
Edwin Perez <edwinperez539@gmail.com>
2016-10-26 07:42:15 -0400

    :beer: Added unit test for react components with jsdom, favicon, and unit test coverage with istanbul

--- generators/app/index.js
+++ generators/app/index.js
@@ -36,6 +36,12 @@ module.exports = yeoman.Base.extend({
 			this.destinationPath('src/app.js')
 		);
 
+		// Sample component
+		this.fs.copy(
+			this.templatePath('src/components/hello-world.js'),
+			this.destinationPath('src/components/hello-world.js')
+		);
+
 		// webpack.config.js
 		this.fs.copy(
 			this.templatePath('webpack.config.js'),
@@ -46,6 +52,18 @@ module.exports = yeoman.Base.extend({
 		this.fs.copy(
 			this.templatePath('.babelrc'),
 			this.destinationPath('.babelrc')
+		);
+
+		// .gitignore
+		this.fs.copy(
+			this.templatePath('.gitignore'),
+			this.destinationPath('.gitignore')
+		);
+
+		// favicon.ico
+		this.fs.copy(
+			this.templatePath('favicon.ico'),
+			this.destinationPath('favicon.ico')
 		);
 
 		// package.json
@@ -67,19 +85,45 @@ module.exports = yeoman.Base.extend({
 			}
 		);
 
+		// UNIT TESTING
+		
+		this.fs.copy(
+			this.templatePath('test/helpers/setup.js'),
+			this.destinationPath('test/helpers/setup.js')
+		);
+
+		this.fs.copy(
+			this.templatePath('test/components/hello-world.spec.js'),
+			this.destinationPath('test/components/hello-world.spec.js')
+		);
+
+		this.fs.copy(
+			this.templatePath('test/mocha.opts'),
+			this.destinationPath('test/mocha.opts')
+		);
+
 	},
 
 	install: function () {
 		this.npmInstall([
 			'react', 
 			'react-dom',
+			'babel',
 			'babel-core',
 			'babel-loader', 
+			'babel-register', 
 			'webpack@beta', 
 			'babel-preset-es2015', 
 			'babel-preset-react', 
 			'babel-preset-stage-0',
-			'serve'
+			'serve',
+			'mocha',
+			'sinon',
+			'chai',
+			'istanbul@1.1.0-alpha.1',
+			'jsdom',
+			'react-addons-test-utils',
+			'enzyme'
 		], {'save-dev': true});
 	}
 });
--- generators/app/templates/src/app.js
+++ generators/app/templates/src/app.js
@@ -1,10 +1,5 @@
 import React, { Component } from 'react';
 import { render }           from 'react-dom';
-
-class HelloWorld extends Component {
-	render () {
-		return <h1>Hello World</h1>;
-	}
-}
+import HelloWorld           from 'components/hello-world';
 
 render(<HelloWorld />, document.getElementById('container'));
