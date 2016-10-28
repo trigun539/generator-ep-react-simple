1943fcba8564cbeb0c9195862cdbf3a68bf8766a
Edwin Perez <edwinperez539@gmail.com>
2016-10-28 10:42:12 -0400

    Fixed webpack resolve issue and cleaned project

--- generators/app/templates/webpack.config.js
+++ generators/app/templates/webpack.config.js
@@ -8,15 +8,19 @@ module.exports = {
 		path: './dist',
 		filename: 'bundle.js'
 	},
-	module: {
-		rules: [
-			{ 
-				test: /\.js$/, 
-				loader: 'babel-loader', 
-				exclude: 'node_modules' 
-			}
+	resolve: {
+		modules: [
+			'node_modules',
+			'./src'
 		]
 	},
-	plugins: [
-	]
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				loader: 'babel-loader',
+				exclude: 'node_modules'
+			}
+		]
+	}
 };
